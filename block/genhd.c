8381fde77967f9d86dcf5b7e950479fd209bdec9
ata-kaner <ata.kaner03@gmail.com>
2024-07-12 09:50:48 +0300

    block: Sync Samsung IOPP with latest block changes
    Change-Id: I9cdb026afaa7e42ae047b79f8f72c7114ddae959

--- block/genhd.c
+++ block/genhd.c
@@ -1144,12 +1144,7 @@ static ssize_t disk_ios_show(struct device *dev,
 	unsigned long discard_ios;
 	unsigned long discard_sectors;
 	long hours;
-	int cpu;
 	int ret;
-
-	cpu = part_stat_lock();
-	part_round_stats(disk->queue, cpu, hd);
-	part_stat_unlock();
 
 	new.ios[STAT_READ] = part_stat_read(hd, ios[STAT_READ]);
 	new.ios[STAT_WRITE] = part_stat_read(hd, ios[STAT_WRITE]);
@@ -1247,13 +1242,8 @@ static ssize_t iobd_show(struct device *dev,
 	struct hd_struct *hd = dev_to_part(dev);
 	struct accumulated_io_stats *old = &(disk->accios);
 	struct accumulated_io_stats new;
-	int cpu;
 	int ret;
 	int idx, sg;
-
-	cpu = part_stat_lock();
-	part_round_stats(disk->queue, cpu, hd);
-	part_stat_unlock();
 
 	for (idx = 0; idx < FSYNC_TIME_GROUP_MAX; idx++)
 		new.fsync_time_cnt[idx] = read_fsync_time_cnt(idx);
@@ -1323,7 +1313,6 @@ static ssize_t iobd_show(struct device *dev,
 
 	return ret;
 }
-
 
 static struct kobject *base_probe(dev_t devt, int *partno, void *data)
 {
@@ -1701,7 +1690,6 @@ static int iostats_show(struct seq_file *seqf, void *v)
 	struct disk_part_iter piter;
 	struct hd_struct *hd;
 	char buf[BDEVNAME_SIZE];
-	int cpu;
 	u64 uptime;
 	unsigned long thresh = 0;
 	unsigned long bg_thresh = 0;
@@ -1712,10 +1700,7 @@ static int iostats_show(struct seq_file *seqf, void *v)
 
 	disk_part_iter_init(&piter, gp, DISK_PITER_INCL_EMPTY_PART0);
 	while ((hd = disk_part_iter_next(&piter))) {
-		cpu = part_stat_lock();
-		part_round_stats(gp->queue, cpu, hd);
 		part_in_flight_rw(gp->queue, hd, inflight);
-		part_stat_unlock();
 		uptime = ktime_to_ns(ktime_get());
 		uptime /= 1000000; /* in ms */
 		bdi = gp->queue->backing_dev_info;
